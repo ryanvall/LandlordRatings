7b46d4164b40c7bfd1b8e789ba5533a228eb1858
Ryan Vall <rav43@case.edu>
2016-08-30 11:52:46 -0400

    added initial landlord and rating models

---
+++ LandlordRatings/src/LandlordRatings/Models/LandlordModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandlordRatings.Models
+{
+    public enum landlordTypes { Individual, Company};
+
+    public class LandlordModel
+    {
+        private landlordTypes type { get; set; }
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+        private string city { get; set; }
+        private string state { get; set; }
+        private string zipcode { get; set; }
+        private RatingModel rating { get; set; }
+    }
+}
---
+++ LandlordRatings/src/LandlordRatings/Models/RatingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandlordRatings.Models
+{
+    public class RatingModel
+    {
+        private int priceScore { get; set; }
+        private int personalityScore { get; set; }
+        private int flexibilityScore { get; set; }
+        private int responsivenessScore { get; set; }
+    }
+}
